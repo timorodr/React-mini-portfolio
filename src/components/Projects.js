45b9c076cbc552b38725835629f8fd7a5e570590
Timothy Rodriguez <tim.rdrgz2017@live.com>
2024-03-26 14:38:38 -0500

    app/code buttons

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { MdArrowOutward } from "react-icons/md";
 
 // the component function
 const Projects = (props) => {
@@ -15,21 +16,25 @@ const Projects = (props) => {
         <div className="projects-container2">
             <img src="https://i.imgur.com/OMifOjL.png" className="project-image" width="200" height="130"/>
             <div className="project-text">
-                <h2 className="project-title"><a href="https://sealproject1-gules.vercel.app/">PokeRumble</a></h2>
-                <p className="project-description"><a href="https://github.com/timorodr/sealproject1" className="project-github">A searchable Pokedex and Pokemon fight club app based on weight class. Based on user weight input, a host of pokemon in your weight class will be the pokemon you would have to battle if you were a pokemon.</a></p>
+                <h2 className="project-title">PokeRumble</h2>
+                <p className="project-description">A retro-style searchable Pokedex(Name or ID) and Pokemon fight club app based on user weight input(+- 5 pounds). A host of pokemon in your weight class will be the pokemon you would have to battle if you were a pokemon.</p>
                 <ul className="project-languages">
                     <li className="project-li">HTML</li>
                     <li className="project-li">CSS</li>
                     <li className="project-li">Javascript</li>
                     <li className="project-li">jQuery</li>
                 </ul>
+                    <div className="code-app-btn-wrapper">
+                        <a href="https://sealproject1-gules.vercel.app/" className="project-github" target="_blank"><button className="btn-github">App</button></a>
+                        <a href="https://github.com/timorodr/sealproject1" className="project-github"><button className="btn-github">Code</button></a>
+                    </div>
             </div>
         </div>
         <div className="projects-container">
             <img src="https://i.imgur.com/UErtUn6.png" className="project-image" width="200" height="130"/>
             <div className="project-text">
-                <h2 className="project-title"><a href="https://project2-crud.onrender.com/">Tattooly: Tattoo Planning App</a></h2>
-                <p className="project-description"><a href="https://github.com/timorodr/Project-2" className="project-github">Tattooly is a tattoo planning app allowing for profile customizability, session saving, and everything needed for your next tattoo appointment! Create an account and begin adding your favorite tattoo reference photos, editing your budget, and updating your calendar for your next appointment. Enjoy, and make this app your canvas! <br></br><br></br>Demo account username:tim14 password:123</a></p>
+                <h2 className="project-title">Tattooly: Tattoo Planning App</h2>
+                <p className="project-description">Tattooly is a tattoo planning app allowing for profile customizability, session saving, and everything needed for your next tattoo appointment! Create an account and begin adding your favorite tattoo reference photos, editing your budget, and updating your calendar for your next appointment. Enjoy, and make this app your canvas! <br></br><br></br>Demo account username:tim14 password:123</p>
                 <ul className="project-languages">
                     <li className="project-li">HTML</li>
                     <li className="project-li">CSS</li>
@@ -37,13 +42,18 @@ const Projects = (props) => {
                     <li className="project-li">Express.js</li>
                     <li className="project-li">MongoDB</li>
                 </ul>
+                    <div className="code-app-btn-wrapper">
+                        <a href="https://project2-crud.onrender.com/" className="project-github" target="_blank"><button className="btn-github">App</button></a>
+                        <a href="https://github.com/timorodr/Project-2" className="project-github"><button className="btn-github">Code</button></a>
+                    </div>
             </div>
         </div>
         <div className="projects-container2">
             <img src="https://i.imgur.com/21pmldV.png" className="project-image" width="200" height="130"/>
             <div className="project-text">
-                <h2 className="project-title"><a href="https://deluxe-trifle-0b2556.netlify.app/">Study Buddy</a></h2>
-                <p className="project-description"><a href="https://github.com/timorodr/MERNstack-FE" className="project-github">Study Buddy is a web application that allows users to create decks of flashcards to help them study anything from software development to foreign languages to trivia. This project was completed in collaboration with 3 other team members.</a></p>
+                <h2 className="project-title">Study Buddy</h2>
+                <p className="project-description">Study Buddy is a web application that allows users to create decks of flashcards to help them study anything from software development to foreign languages to trivia. This project was completed in collaboration with 3 other team members.
+                </p>
                 <ul className="project-languages">
                     <li className="project-li">MongoDB</li>
                     <li className="project-li">Express.js</li>
@@ -51,6 +61,10 @@ const Projects = (props) => {
                     <li className="project-li">NodeJS</li>
                     <li className="project-li">CSS</li>
                 </ul>
+                    <div className="code-app-btn-wrapper">
+                        <a href="https://deluxe-trifle-0b2556.netlify.app/" className="project-github" target="_blank"><button className="btn-github">App</button></a>
+                        <a href="https://github.com/timorodr/MERNstack-FE" className="project-github"><button className="btn-github">Code</button></a>
+                    </div>
             </div>
         </div>
     </div>
