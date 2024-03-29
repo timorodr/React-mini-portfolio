25323983be4c38e798ecfefd0d072fad6b105c4b
Timothy Rodriguez <tim.rdrgz2017@live.com>
2024-03-29 08:51:09 -0500

    adding target attributes

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,9 @@ const Header = (props) => {
         <nav>
             <ul className="header-ul">
                 <li className="header-li"><a href="#aboutme">About</a></li>
-                <li className="header-li"><a href="https://github.com/timorodr">GitHub</a></li>
-                <li className="header-li"><a href="https://www.linkedin.com/in/timothy-rodriguez-swe/">LinkedIn</a></li>
-                <li className="header-li"><a href="https://docs.google.com/document/d/1RFiD9BfOOwi-aQmA1ExnzfzXSRBeyFMUq-isewrs8N0/edit?usp=sharing">Resume</a></li>
+                <li className="header-li"><a href="https://github.com/timorodr" target="_blank">GitHub</a></li>
+                <li className="header-li"><a href="https://www.linkedin.com/in/timothy-rodriguez-swe/" target="_blank">LinkedIn</a></li>
+                <li className="header-li"><a href="https://docs.google.com/document/d/1RFiD9BfOOwi-aQmA1ExnzfzXSRBeyFMUq-isewrs8N0/edit?usp=sharing" target="_blank">Resume</a></li>
             </ul>
         </nav>
     </div>
