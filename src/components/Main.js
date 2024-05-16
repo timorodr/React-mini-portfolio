e3d93bd5bd75f38df4e1c453f7a76d7d42322d6e
Timothy Rodriguez <tim.rdrgz2017@live.com>
2024-05-15 22:00:49 -0500

    add resume and color links

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,14 +17,14 @@ const Main = (props) => {
               <div class="row flex-nowrap justify-content-center align-items-center">
             
                 <div class="col-4 text-center">
-                  <a class="blog-header-logo text-body-emphasis text-decoration-none" href="#">Welcome!</a>
+                  <a class="blog-header-logo text-decoration-none" style={{color: "#e8d1c5"}}>Welcome!</a>
                 </div>
             
               </div>
             </header>
-              <div class="nav-scroller py-1 mb-3 border-bottom">
+              <div class="nav-scroller py-1 mb-3 border-bottom" id="nav">
                 <nav class="nav nav-underline justify-content-between">
-                  <a class="nav-item nav-link link-body-emphasis active" href="#aboutme">About</a>
+                  <a class="nav-item nav-link link-body-emphasis" href="#aboutme">About</a>
                   <a class="nav-item nav-link link-body-emphasis" href="#projects">Projects</a>
                   <a class="nav-item nav-link link-body-emphasis" href="#skills">Skills</a>
                   <a class="nav-item nav-link link-body-emphasis" href="#contact">Contact</a>
@@ -34,7 +34,7 @@ const Main = (props) => {
             </div>
           
             <main class="container">
-              <div class="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary" id="aboutme">
+              <div class="p-4 p-md-5 mb-4 rounded text-body-emphasis" id="aboutme">
                 <div class="row"> 
                   <div class="col-lg-6 px-0">
                     <h1 class="display-4 fst-italic">Timothy Rodriguez</h1>
@@ -52,29 +52,29 @@ const Main = (props) => {
 
       <br></br>
       
-            <h1 class="display-4 fst-italic text-center" id="projects">Projects</h1>
+            <h1 class="display-4 fst-italic text-center" id="projects" style={{color: "#e8d1c5"}}>Projects</h1>
             <div class="row mb-2">
               <div class="col-md-12">
-                <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative" style={{backgroundColor: "#B1B5A6"}}>
                   <div class="col-auto d-none d-lg-block">
                     <img class="bd-placeholder-img" width="350" height="250" src="https://i.imgur.com/xEBA7Dp.png" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
                   </div>
                   <div class="col p-4 d-flex flex-column position-static">
-                    <strong class="d-inline-block mb-2 text-primary-emphasis">Wellness App - Demo account username:demoacct@demo.com password:demoacct</strong>
+                    <strong class="d-inline-block mb-2" style={{color: "#000000"}}>Wellness App - Demo account username:demoacct@demo.com password:demoacct</strong>
                     <h3 class="mb-0"><b>MediManage</b></h3>
                     <div class="mb-1 text-body-secondary" style={{display: "flex", justifyContent: "start", alignItems: "center", listStyleType: "none"}}>
-                        <li style={{marginRight: 12, color: "red"}}>Golang</li>
-                        <li style={{marginRight: 12}}>React</li>
-                        <li style={{marginRight: 12}}>MongoDB</li>
-                        <li style={{marginRight: 12}}>Bootstrap</li>
+                        <li style={{marginRight: 12, color: "#FCF7FF"}}>Golang</li>
+                        <li style={{marginRight: 12, color: "#FCF7FF"}}>React</li>
+                        <li style={{marginRight: 12, color: "#FCF7FF"}}>MongoDB</li>
+                        <li style={{marginRight: 12, color: "#FCF7FF"}}>Bootstrap</li>
                     </div>
                     <p class="card-text mb-auto">A medication management application for improved health, reduced side effects, fewer medication errors, and increased sense of control over health.</p>
                     
                     <div class="p-1">
-                      <a href="#" class="icon-link gap-1 icon-link-hover" style={{marginRight: 12}}>
+                      <a href="https://main--medimanage.netlify.app/" target="_blank" class="icon-link gap-1 icon-link-hover" style={{marginRight: 12}}>
                         View Project
                       </a>
-                      <a href="#" class="icon-link gap-1 icon-link-hover">
+                      <a href="https://github.com/timorodr/go-react-final-BE" target="_blank" class="icon-link gap-1 icon-link-hover">
                         View Code
                       </a>
                     </div>
@@ -83,15 +83,15 @@ const Main = (props) => {
               </div>
 
               <div class="col-md-12">
-                <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative" style={{backgroundColor: "#B1B5A6"}}>
                   <div class="col p-4 d-flex flex-column position-static">
-                    <strong class="d-inline-block mb-2 text-success-emphasis">Flashcard Study App</strong>
+                    <strong class="d-inline-block mb-2" style={{color: "#000000"}}>Flashcard Study App</strong>
                     <h3 class="mb-0"><b>StudyBuddy</b></h3>
                     <div class="mb-1 text-body-secondary" style={{display: "flex", justifyContent: "start", alignItems: "center", listStyleType: "none"}}>
-                        <li style={{marginRight: 12}}>MongoDB</li>
-                        <li style={{marginRight: 12}}>Express</li>
-                        <li style={{marginRight: 12}}>React</li>
-                        <li style={{marginRight: 12}}>NodeJS</li>
+                        <li style={{marginRight: 12, color: "#FCF7FF"}}>MongoDB</li>
+                        <li style={{marginRight: 12, color: "#FCF7FF"}}>Express</li>
+                        <li style={{marginRight: 12, color: "#FCF7FF"}}>React</li>
+                        <li style={{marginRight: 12, color: "#FCF7FF"}}>NodeJS</li>
                     </div>
                     <p class="mb-auto">Study Buddy is a web app for creating flashcard decks, ideal for studying software development, foreign languages, trivia, and more, developed with a team of 4 members.</p>
                     <div class="p-1">
@@ -110,20 +110,20 @@ const Main = (props) => {
               </div>
 
               <div class="col-md-12">
-                <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative" style={{backgroundColor: "#B1B5A6"}}>
                   <div class="col-auto d-none d-lg-block">
                     <img class="bd-placeholder-img" width="350" height="250" src="https://i.imgur.com/UErtUn6.png" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
                   </div>
                   <div class="col p-4 d-flex flex-column position-static">
-                    <strong class="d-inline-block mb-2 text-primary-emphasis">Tattoo Planning App - Demo account username:tim14 password:123</strong>
+                    <strong class="d-inline-block mb-2" style={{color: "#000000"}}>Tattoo Planning App - Demo account username:tim14 password:123</strong>
                     <h3 class="mb-0"><b>Tattooly</b></h3>
                     <div class="mb-1 text-body-secondary" style={{display: "flex", justifyContent: "start", alignItems: "center", listStyleType: "none"}}>
-                        <li style={{marginRight: 12, color: "red"}}>HTML</li>
-                        <li style={{marginRight: 12}}>CSS</li>
-                        <li style={{marginRight: 12}}>Javascript</li>
-                        <li style={{marginRight: 12}}>MongoDB</li>
-                        <li style={{marginRight: 12}}>Express</li>
-                        <li style={{marginRight: 12}}>EJS</li>
+                        <li style={{marginRight: 12, color: "#FCF7FF"}}>HTML</li>
+                        <li style={{marginRight: 12, color: "#FCF7FF"}}>CSS</li>
+                        <li style={{marginRight: 12, color: "#FCF7FF"}}>Javascript</li>
+                        <li style={{marginRight: 12, color: "#FCF7FF"}}>MongoDB</li>
+                        <li style={{marginRight: 12, color: "#FCF7FF"}}>Express</li>
+                        <li style={{marginRight: 12, color: "#FCF7FF"}}>EJS</li>
                     </div>
                     <p class="card-text mb-auto">Tattooly simplifies tattoo planning, allowing users to customize profiles, save sessions, and manage appointments. Join now to turn your ideas into reality and make Tattooly your ultimate canvas!</p>
                     
@@ -140,15 +140,15 @@ const Main = (props) => {
               </div>
               
               <div class="col-md-12">
-                <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative" style={{backgroundColor: "#B1B5A6"}}>
                   <div class="col p-4 d-flex flex-column position-static">
-                    <strong class="d-inline-block mb-2 text-success-emphasis">Retro Pokemon App</strong>
+                    <strong class="d-inline-block mb-2" style={{color: "#000000"}}>Retro Pokemon App</strong>
                     <h3 class="mb-0"><b>PokeRumble</b></h3>
                     <div class="mb-1 text-body-secondary" style={{display: "flex", justifyContent: "start", alignItems: "center", listStyleType: "none"}}>
-                        <li style={{marginRight: 12}}>HTML</li>
-                        <li style={{marginRight: 12}}>CSS</li>
-                        <li style={{marginRight: 12}}>Javascript</li>
-                        <li style={{marginRight: 12}}>jQuery</li>
+                        <li style={{marginRight: 12, color: "#FCF7FF"}}>HTML</li>
+                        <li style={{marginRight: 12, color: "#FCF7FF"}}>CSS</li>
+                        <li style={{marginRight: 12, color: "#FCF7FF"}}>Javascript</li>
+                        <li style={{marginRight: 12, color: "#FCF7FF"}}>jQuery</li>
                     </div>
                     <p class="mb-auto">A retro-style searchable Pokedex(Name or ID) and Pokemon fight club app based on user weight input(+/- 5 pounds). A host of pokemon in your weight class will be the pokemon you would have to battle if you were a pokemon.</p>
                     <div class="p-1">
@@ -178,7 +178,7 @@ const Main = (props) => {
                 </h4>
           
                 {/* <article class="blog-post"> */}
-          <h2 class="display-5 link-body-emphasis mb-1" id="skills">Technical Skills</h2>
+          <h2 class="display-5 mb-1" id="skills" style={{color: "#e8d1c5"}}>Technical Skills</h2>
           <hr></hr>
             <div className="skills-container">
               <h3>Javascript</h3>
@@ -248,7 +248,7 @@ const Main = (props) => {
                 </article>*/}
           
                 <article class="blog-post"> 
-          <h2 class="display-5 link-body-emphasis mb-1">Education</h2>
+          <h2 class="display-5 mb-1" style={{color: "#e8d1c5"}}>Education</h2>
           <hr></hr>
           {/* <p class="blog-post-meta">December 23, 2020 by <a href="#">Jacob</a></p> */}
           <h3><b>General Assembly Software Engineering Immersive</b></h3>
@@ -304,7 +304,7 @@ const Main = (props) => {
                 </article>
           
                 <article class="blog-post">
-          <h2 class="display-5 link-body-emphasis mb-1">Slice of Life</h2>
+          <h2 class="display-5 mb-1" style={{color: "#e8d1c5"}}>Slice of Life</h2>
           <hr></hr>
           {/* <p class="blog-post-meta">December 14, 2020 by <a href="#">Chris</a></p> */}
           <blockquote>
@@ -342,16 +342,16 @@ const Main = (props) => {
           
               <div class="col-md-4">
                 <div class="position-sticky" style={{top: 2}}>
-                  <div class="p-4 mb-3 bg-body-tertiary rounded">
-                    <h4 class="fst-italic">About</h4>
+                  <div class="p-4 mb-3 rounded" id="connect">
+                    <h4 class="fst-italic" style={{color: "#D0A48A"}}>About</h4>
                     <p class="mb-0">I build immersive, creative, and accessible digital playgrounds.</p>
                   </div>
                   <div>
-                    <h1 class="fst-italic" id="contact">Lets Connect!</h1>
+                    <h1 class="fst-italic" id="contact" style={{color: "#e8d1c5"}}>Lets Connect!</h1>
                     <ul class="list-unstyled">
                       <li>
                         <a class="d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center py-3 link-body-emphasis text-decoration-none border-top" href="https://www.linkedin.com/in/timothy-rodriguez-swe/" target="_blank">
-                        <li style={{height: 60}} aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><a href="https://www.linkedin.com/in/timothy-rodriguez-swe/" target="_blank"><FontAwesomeIcon icon={faLinkedin} style={{height: 60, color: "black"}}/></a></li>
+                        <li style={{height: 60}} aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><a href="https://www.linkedin.com/in/timothy-rodriguez-swe/" target="_blank"><FontAwesomeIcon icon={faLinkedin} style={{height: 60, color: "#FCF7FF"}}/></a></li>
                           {/* <svg class="bd-placeholder-img" width="100%" height="96" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg> */}
                           <div class="col-lg-8">
                             <h3 class="mb-0">LinkedIn</h3>
@@ -361,7 +361,7 @@ const Main = (props) => {
                       </li>
                       <li>
                         <a class="d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center py-3 link-body-emphasis text-decoration-none border-top" href="https://github.com/timorodr" target="_blank">
-                        <li style={{height: 60}} aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><a href="https://github.com/timorodr" target="_blank"><FontAwesomeIcon icon={faGithub} style={{height: 60, color: "black"}}/></a></li>
+                        <li style={{height: 60}} aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><a href="https://github.com/timorodr" target="_blank"><FontAwesomeIcon icon={faGithub} style={{height: 60, color: "#FCF7FF"}}/></a></li>
                           {/* <img class="bd-placeholder-img" width="100%" height="96" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS4UJP8NHuKjQ34J-SqrUsOkUn4LgI4TQwFA3vAtqkvw&s" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"></img> */}
                           <div class="col-lg-8">
                             <h3 class="mb-0">Github</h3>
@@ -371,7 +371,7 @@ const Main = (props) => {
                       </li>
                       <li>
                       <a class="d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center py-3 link-body-emphasis text-decoration-none border-top" href="mailto: timothyjayrodriguez@gmail.com" target="_blank">
-                        <li style={{height: 60}} aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><a href="mailto: timothyjayrodriguez@gmail.com" target="_blank"><FontAwesomeIcon icon={faEnvelope} style={{height: 60, color: "black"}}/></a></li>
+                        <li style={{height: 60}} aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><a href="mailto: timothyjayrodriguez@gmail.com" target="_blank"><FontAwesomeIcon icon={faEnvelope} style={{height: 60, color: "#FCF7FF"}}/></a></li>
                           {/* <svg class="bd-placeholder-img" width="100%" height="96" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg> */}
                           <div class="col-lg-8">
                             <h3 class="mb-0">Email</h3>
@@ -412,7 +412,7 @@ const Main = (props) => {
           
           </main>
 
-          <footer class="py-5 text-center text-body-secondary bg-body-tertiary">
+          <footer class="py-5 text-center text-body-secondary" id="footer">
             <p>Created by Timothy Rodriguez<a href="https://www.linkedin.com/in/timothy-rodriguez-swe/" target="_blank"><FontAwesomeIcon icon={faLinkedin} style={{color: "black", marginLeft: 10}}/></a><a href="https://github.com/timorodr" target="_blank"><FontAwesomeIcon icon={faGithub} style={{color: "black", marginLeft: 10}}/></a><a href="mailto: timothyjayrodriguez@gmail.com" target="_blank"><FontAwesomeIcon icon={faEnvelope} style={{color: "black", marginLeft: 10}}/></a></p>
             <p class="mb-0">
               <a href="#">Back to top</a>
